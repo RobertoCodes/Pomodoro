45c8e2f7ef453310d1e2fa140b9263ee64ae977d
Robert <robertandrewromano@gmail.com>
2016-02-07 23:34:39 -0500

    Add create method to Todo controller.

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,2 +1,16 @@
 class Api::TodosController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render :show
+    else
+      render json: @todo.errors.full_messages
+    end
+  end
+
+  
 end
