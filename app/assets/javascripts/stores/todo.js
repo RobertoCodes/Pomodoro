7bb4ea27b5dfda91cfc565e346d94c82d82fe890
Robert <robertandrewromano@gmail.com>
2016-02-08 21:13:58 -0500

    Add resetTodos function to Todo Store.

--- app/assets/javascripts/stores/todo.js
+++ app/assets/javascripts/stores/todo.js
@@ -2,6 +2,10 @@
 
   var _todos = [];
 
+  var resetTodos = function (todos) {
+    _todos = todos;
+  };
+
   window.TodoStore = $.extend({}, EventEmitter.prototype, {
     all: function () {
       return _todos.slice();
