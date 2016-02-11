ccac14cc6d3d857c9899bd0517d2e04678787949
Robert <robertandrewromano@gmail.com>
2016-02-10 23:20:01 -0500

    Add click handler to expand TodosIndexItem.

--- app/assets/javascripts/components/todos/TodosIndexItem.js.jsx
+++ app/assets/javascripts/components/todos/TodosIndexItem.js.jsx
@@ -1,10 +1,19 @@
 window.TodosIndexItem = React.createClass({
 
+  getInitialState: function () {
+    this.setState({expanded: false});
+  },
+
+  handleClick: function () {
+    expanded = this.state.expanded;
+    this.setState({expanded: !expanded});
+  },
+
   render: function () {
     return (
-      <div>
-        <li>{this.props.todo.name} + {this.props.todo.pomodoros}</li>
-      </div>
+      <li onClick={this.handleClick}>
+        {this.props.todo.name} + {this.props.todo.pomodoros}
+      </li>
     )
   }
 
