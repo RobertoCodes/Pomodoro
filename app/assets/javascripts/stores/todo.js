e937f51412451dceb9a54da5508998146d5fb611
Robert <robertandrewromano@gmail.com>
2016-02-11 23:37:29 -0500

    Add TODO RECEIVED case to dispatcher and add resetTodo function.

--- app/assets/javascripts/stores/todo.js
+++ app/assets/javascripts/stores/todo.js
@@ -8,6 +8,17 @@
     _todos = todos;
   };
 
+  var resetTodo = function (todo) {
+    var switched = false;
+    _todos.forEach(function (td) {
+      if (td.id === todo.id) {
+        _todos[_todos.indexOf(td)] = todo;
+        switched = true;
+      }
+    });
+    if(!switched) {_todos.push(todo);}
+  };
+
   window.TodoStore = $.extend({}, EventEmitter.prototype, {
     all: function () {
       return _todos.slice();
@@ -20,9 +31,13 @@
     dispatcherID: AppDispatcher.register(function (payload) {
       switch(payload.actionType) {
         case TodoConstants.TODOS_RECEIVED:
-        resetTodos(payload.todos);
-        TodoStore.emit(TODOS_INDEX_CHANGE_EVENT);
+          resetTodos(payload.todos);
+          TodoStore.emit(TODOS_INDEX_CHANGE_EVENT);
         break;
+        case TodoConstants.TODO_RECEIVED:
+          resetTodo(payload.todo);
+          TodoStore.emit(TODOS_INDEX_CHANGE_EVENT);
+
     }
     })
 
