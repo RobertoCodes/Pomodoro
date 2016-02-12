8e32b0573b28862a96c58e8f3b82dd446ce49e23
Robert <robertandrewromano@gmail.com>
2016-02-11 23:27:40 -0500

    Add receiveOneTodo function to ApiActions.

--- app/assets/javascripts/actions/api_actions.js
+++ app/assets/javascripts/actions/api_actions.js
@@ -2,8 +2,15 @@ window.ApiActions = {
 
   receiveAllTodos: function (todos) {
     AppDispatcher.dispatch({
-    actionType: TodoConstants.TODOS_RECEIVED,
-    todos: todos
+      actionType: TodoConstants.TODOS_RECEIVED,
+      todos: todos
+    });
+  },
+
+  receiveOneTodo: function (todo) {
+    AppDispatcher.dispatch({
+      actionType: TodoConstants.TODO_RECEIVED,
+      todo: todo
     });
   }
 
