7eb6d05b4ad2356e86d33e93f98c96838f9326d4
Robert <robertandrewromano@gmail.com>
2016-02-12 00:31:25 -0500

    Add TodoEditForm component with similar functions to TodoForm.

---
+++ app/assets/javascripts/components/todos/TodoEditForm.js.jsx
@@ -0,0 +1,57 @@
+window.TodoEditForm = React.createClass({
+
+  getInitialState: function () {
+    return ({ name: this.props.todo.name, pomodoros: this.props.todo.pomodoros, revealed: true});
+  },
+
+  handleNumChange: function (e) {
+    this.setState({pomodoros: e.target.value});
+  },
+
+  handleNameChange: function (e) {
+    this.setState({name: e.target.value});
+  },
+
+  editTodo: function (e) {
+    e.preventDefault();
+    var todo = {};
+    Object.keys(this.state).forEach(function (key) {
+      todo[key] = this.state[key];
+    }.bind(this));
+    this.setState({ name: "", pomodoros: "", revealed: false});
+    debugger;
+    ApiUtil.editTodo(todo);
+  },
+
+  render: function () {
+
+    if (!this.state.revealed) {
+      return (
+        <div id="todo-form">
+          <form onSubmit={this.createTodo}>
+            <input id="todo-name" value={this.state.name}
+              onChange={this.handleNameChange} placeholder="What do you need to do?"/>
+            <br/>
+              <input id="pomodoro-number" type="number" min="1" max="100" onChange={this.handleNumChange} value={this.state.pomodoros}/>
+            <br/>
+            <button id="add-todo-button">Add Todo</button>
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div id="todo-form">
+          <form onSubmit={this.createTodo}>
+            <input id="todo-name" value={this.state.name}
+              onChange={this.handleNameChange} placeholder="What do you need to do?"/>
+            <br/>
+              <input id="pomodoro-number" type="number" min="1" max="100" onChange={this.handleNumChange} value={this.state.pomodoros}/>
+            <br/>
+            <button id="add-todo-button">Add Todo</button>
+          </form>
+        </div>
+       );
+      }
+    }
+
+});
